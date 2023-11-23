02ab43a1b962d7804b98dff1b4de3767b0bd45d0
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-24 00:57:01 +0530

    Adding Address

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.*;
+class Address{
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String email;
+	
+	Address(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+}
 
 public class Main {
 	public static void main(String args[]) {
+		ArrayList<Address> addressBook = new ArrayList<Address>();
 		System.out.println("Welcome to Address Book");
 	}
 }
