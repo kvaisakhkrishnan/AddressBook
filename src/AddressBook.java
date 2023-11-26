f9afef41516cade20f867090840add7cdb11ebf7
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-26 17:19:53 +0530

    Multiple Address Books Added

---
+++ src/AddressBook.java
@@ -0,0 +1,105 @@
+import java.util.*;
+
+public class AddressBook {
+	private ArrayList<Address> addressList;
+	
+	AddressBook(){
+		addressList = new ArrayList<Address>();
+	}
+	
+	public ArrayList<Address> getAddressList() {
+		return addressList;
+	}
+
+	public void setAddressList(ArrayList<Address> addressList) {
+		this.addressList = addressList;
+	}
+
+	public void addAddress() {
+		System.out.println("--------------ADD--------------");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Firstname: ");
+		String firstName = scanner.nextLine();
+		System.out.println("Enter Lastname: ");
+		String lastName = scanner.nextLine();
+		System.out.println("Enter Address: ");
+		String address = scanner.nextLine();
+		System.out.println("Enter City: ");
+		String city = scanner.nextLine();
+		System.out.println("Enter State: ");
+		String state = scanner.nextLine();
+		System.out.println("Enter Zip: ");
+		String zip = scanner.nextLine();
+		System.out.println("Enter Phonenumber: ");
+		String phoneNumber = scanner.nextLine();
+		System.out.println("Enter Email: ");
+		String email = scanner.nextLine();
+		addressList.add(new Address(firstName, lastName, address, city, state, zip, phoneNumber, email));
+		System.out.println("Address Added");
+	}
+	
+	public void editAddress() {
+		System.out.println("--------------UPDATE--------------");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Firstname: ");
+		String firstName = scanner.nextLine();
+		System.out.println("Enter Lastname: ");
+		String lastName = scanner.nextLine();
+		System.out.println("Enter New Address: ");
+		String address = scanner.nextLine();
+		System.out.println("Enter New City: ");
+		String city = scanner.nextLine();
+		System.out.println("Enter New State: ");
+		String state = scanner.nextLine();
+		System.out.println("Enter New Zip: ");
+		String zip = scanner.nextLine();
+		System.out.println("Enter New Phonenumber: ");
+		String phoneNumber = scanner.nextLine();
+		System.out.println("Enter New Email: ");
+		String email = scanner.nextLine();
+		System.out.println("Searching Address Book");
+		int flag = 0;
+		for(int i = 0; i < addressList.size(); i++) {
+			if(addressList.get(i).getFirstName().equals(firstName) && addressList.get(i).getLastName().equals(lastName)) {
+				flag = 1;
+				addressList.get(i).setAddress(address);
+				addressList.get(i).setCity(city);
+				addressList.get(i).setState(state);
+				addressList.get(i).setZip(zip);
+				addressList.get(i).setPhoneNumber(phoneNumber);
+				addressList.get(i).setEmail(email);
+				break;
+			}
+		}
+		if(flag == 1) {
+			System.out.println("Updated Address");
+		}
+		else {
+			System.out.println("Address Not Found");
+		}
+	}
+	
+	public void deleteAddress() {
+		System.out.println("--------------DELETE--------------");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Firstname: ");
+		String firstName = scanner.nextLine();
+		System.out.println("Enter Lastname: ");
+		String lastName = scanner.nextLine();
+		System.out.println("Searching Address Book");
+		int flag = 0;
+		for(int i = 0; i < addressList.size(); i++) {
+			if(addressList.get(i).getFirstName().equals(firstName) && addressList.get(i).getLastName().equals(lastName)) {
+				flag = 1;
+				addressList.remove(i);
+				break;
+			}
+		}
+		if(flag == 1) {
+			System.out.println("Deleted Address");
+		}
+		else {
+			System.out.println("Address Not Found");
+		}
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,195 +1,54 @@
 import java.util.*;
-class Address{
-	private String firstName;
-	public String getFirstName() {
-		return firstName;
-	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getZip() {
-		return zip;
-	}
-
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	private String lastName;
-	private String address;
-	private String city;
-	private String state;
-	private String zip;
-	private String phoneNumber;
-	private String email;
-	
-	Address(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email){
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.zip = zip;
-		this.phoneNumber = phoneNumber;
-		this.email = email;
-	}
-}
 
 public class Main {
-	public static void addAddress(ArrayList<Address> addressBook) {
-		System.out.println("--------------ADD--------------");
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter Firstname: ");
-		String firstName = scanner.nextLine();
-		System.out.println("Enter Lastname: ");
-		String lastName = scanner.nextLine();
-		System.out.println("Enter Address: ");
-		String address = scanner.nextLine();
-		System.out.println("Enter City: ");
-		String city = scanner.nextLine();
-		System.out.println("Enter State: ");
-		String state = scanner.nextLine();
-		System.out.println("Enter Zip: ");
-		String zip = scanner.nextLine();
-		System.out.println("Enter Phonenumber: ");
-		String phoneNumber = scanner.nextLine();
-		System.out.println("Enter Email: ");
-		String email = scanner.nextLine();
-		addressBook.add(new Address(firstName, lastName, address, city, state, zip, phoneNumber, email));
-		System.out.println("Address Added");
-	}
-	public static void editAddress(ArrayList<Address> addressBook) {
-		System.out.println("--------------UPDATE--------------");
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter Firstname: ");
-		String firstName = scanner.nextLine();
-		System.out.println("Enter Lastname: ");
-		String lastName = scanner.nextLine();
-		System.out.println("Enter New Address: ");
-		String address = scanner.nextLine();
-		System.out.println("Enter New City: ");
-		String city = scanner.nextLine();
-		System.out.println("Enter New State: ");
-		String state = scanner.nextLine();
-		System.out.println("Enter New Zip: ");
-		String zip = scanner.nextLine();
-		System.out.println("Enter New Phonenumber: ");
-		String phoneNumber = scanner.nextLine();
-		System.out.println("Enter New Email: ");
-		String email = scanner.nextLine();
-		System.out.println("Searching Address Book");
-		int flag = 0;
-		for(int i = 0; i < addressBook.size(); i++) {
-			if(addressBook.get(i).getFirstName().equals(firstName) && addressBook.get(i).getLastName().equals(lastName)) {
-				flag = 1;
-				addressBook.get(i).setAddress(address);
-				addressBook.get(i).setCity(city);
-				addressBook.get(i).setState(state);
-				addressBook.get(i).setZip(zip);
-				addressBook.get(i).setPhoneNumber(phoneNumber);
-				addressBook.get(i).setEmail(email);
-				break;
-			}
-		}
-		if(flag == 1) {
-			System.out.println("Updated Address");
-		}
-		else {
-			System.out.println("Address Not Found");
-		}
-	}
-	public static void deleteAddress(ArrayList<Address> addressBook) {
-		System.out.println("--------------DELETE--------------");
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter Firstname: ");
-		String firstName = scanner.nextLine();
-		System.out.println("Enter Lastname: ");
-		String lastName = scanner.nextLine();
-		System.out.println("Searching Address Book");
-		int flag = 0;
-		for(int i = 0; i < addressBook.size(); i++) {
-			if(addressBook.get(i).getFirstName().equals(firstName) && addressBook.get(i).getLastName().equals(lastName)) {
-				flag = 1;
-				addressBook.remove(i);
-				break;
-			}
-		}
-		if(flag == 1) {
-			System.out.println("Deleted Address");
-		}
-		else {
-			System.out.println("Address Not Found");
-		}
-	}
+	
 	public static void main(String args[]) {
-		ArrayList<Address> addressBook = new ArrayList<Address>();
+		Dictionary<String, AddressBook> library = new Hashtable<>();
+		AddressBook addressBook = new AddressBook();
 		System.out.println("Welcome to Address Book");
 		while(true) {
-			System.out.println("1. Add address\n2. Update Address\n3. Delete Address\4.Exit");
+			System.out.println("1. Add Address Book\n2. Add address\n3. Update Address\n4. Delete Address\n5. Exit");
 			int count;
 			Scanner scanner = new Scanner(System.in);
 			count = scanner.nextInt();
-			
 			if(count == 1) {
-				addAddress(addressBook);
-				
+				System.out.println("Give Address Book Name: ");
+				String addressBookName = scanner.next();
+				library.put(addressBookName, new AddressBook());
+				System.out.println("New Address Book Added");
 			}
 			else if(count == 2) {
-				editAddress(addressBook);
+				System.out.println("Enter Address Book Name: ");
+				String addressBookName = scanner.next();
+				if(library.get(addressBookName) != null) {
+					library.get(addressBookName).addAddress();
+				}
+				else {
+					System.out.println("Address Book Now Found");
+				}
 				
 			}
 			else if(count == 3) {
-				deleteAddress(addressBook);
+				System.out.println("Enter Address Book Name: ");
+				String addressBookName = scanner.next();
+				if(library.get(addressBookName) != null) {
+					library.get(addressBookName).editAddress();
+				}
+				else {
+					System.out.println("Address Book Now Found");
+				}
+				
+			}
+			else if(count == 4) {
+				System.out.println("Enter Address Book Name: ");
+				String addressBookName = scanner.next();
+				if(library.get(addressBookName) != null) {
+					library.get(addressBookName).deleteAddress();
+				}
+				else {
+					System.out.println("Address Book Now Found");
+				}
 			}
 			else {
 				break;
